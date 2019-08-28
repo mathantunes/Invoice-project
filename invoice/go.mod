ab97b43dbdedbf0f77dc1fa7790173c33b8e5a2e
JESUS Matheus <matheus.jesus@alstomgroup.com>
2019-08-27 23:22:29 -0300

    storage v0.0.0

--- invoice/go.mod
+++
@@ -1,9 +0,0 @@
-module invoice
-
-go 1.12
-
-require (
-	github.com/golang/protobuf v1.3.2 // indirect
-	github.com/mathantunes/arex_project v0.0.0-20190826232521-135563293ac9
-	google.golang.org/grpc v1.23.0 // indirect
-)
---
+++ storage/scripts/createTable.sql
@@ -0,0 +1,25 @@
+
+-- SQL Script for Creating AP and AR invoice tables
+CREATE TABLE IF NOT EXISTS ap_invoices (
+    customer_id UUID NOT NULL,
+    invoice_number INTEGER NOT NULL,
+    currency TEXT NOT NULL,
+    face_value INTEGER NOT NULL,
+    counterparty_vat TEXT NOT NULL,
+    issue_date TEXT NOT NULL,
+    due_date TEXT NOT NULL,
+
+    PRIMARY KEY (counterparty_vat, invoice_number)
+);
+
+CREATE TABLE IF NOT EXISTS ar_invoices (
+    customer_id UUID NOT NULL, 
+    invoice_number INTEGER NOT NULL,
+    currency TEXT NOT NULL,
+    face_value INTEGER NOT NULL,
+    counterparty_vat TEXT NOT NULL,
+    issue_date TEXT NOT NULL,
+    due_date TEXT NOT NULL,
+    
+    PRIMARY KEY (customer_id, invoice_number)
+);

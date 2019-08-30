b1fe0c17feae624468c0b0d5c5421e222bffd4af
JESUS Matheus <matheus.jesus@alstomgroup.com>
2019-08-29 23:56:49 -0300

    validator 0.0.5

--- validator/validator_test.go
+++ validator/validator_test.go
@@ -5,46 +5,50 @@ import (
 	"testing"
 )
 
-func TestValidate(t *testing.T) {
+func TestVIESValidator_Validate(t *testing.T) {
 	type args struct {
 		countryCode string
 		vatNumber   string
 	}
 	tests := []struct {
 		name    string
+		v       *VIESValidator
 		args    args
-		want    bool
+		want    InternalResponse
 		wantErr bool
 	}{
 		{
-			name:    "Valid Request from FINLAND",
-			args:    args{"FI", "25160553"},
-			want:    true,
+			name: "Valid Request from FINLAND",
+			v:    &VIESValidator{},
+			args: args{"FI", "25160553"},
+			want: InternalResponse{
+				Valid:       true,
+				CompanyName: "Comtower Finland Oy",
+			},
 			wantErr: false,
 		},
 		{
 			name:    "Invalid VAT Request from FINLAND",
 			args:    args{"FI", "000000"},
-			want:    false,
+			want:    InternalResponse{Valid: false, CompanyName: "---"},
 			wantErr: false,
 		},
 		{
 			name:    "Invalid Parameters on Request",
 			args:    args{"", "0"},
-			want:    false,
+			want:    InternalResponse{},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			v := &VIESValidator{}
-			got, err := v.Validate(tt.args.countryCode, tt.args.vatNumber)
+			got, err := tt.v.Validate(tt.args.countryCode, tt.args.vatNumber)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("VIESValidator.Validate() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Validate() = %v, want %v", got, tt)
+				t.Errorf("VIESValidator.Validate() = %v, want %v", got, tt.want)
 			}
 		})
 	}

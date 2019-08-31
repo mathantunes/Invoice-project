9e2430c40a0873054bbe43903900b881456c08e1
JESUS Matheus <matheus.jesus@alstomgroup.com>
2019-08-31 18:57:44 -0300

    filestore

--- filestore/filestore_test.go
+++ filestore/filestore_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -26,6 +27,7 @@ func TestFileManager_CreateBucket(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		endpoint = "localhost:4572"
 		t.Run(tt.name, func(t *testing.T) {
 			if err := tt.f.CreateBucket(tt.args.name); (err != nil) != tt.wantErr {
 				t.Errorf("FileManager.CreateBucket() error = %v, wantErr %v", err, tt.wantErr)
@@ -63,8 +65,14 @@ func TestFileManager_Upload(t *testing.T) {
 			f:    New(),
 			args: args{"preview_bucket", "testfile", bytes.NewReader(readFile("../samples/invoice_preview.pdf"))},
 		},
+		{
+			name: "Upload to S3",
+			f:    New(),
+			args: args{"preview_bucket", "test/1", bytes.NewReader(readFile("../samples/invoice_preview.pdf"))},
+		},
 	}
 	for _, tt := range tests {
+		endpoint = "localhost:4572"
 		t.Run(tt.name, func(t *testing.T) {
 			if err := tt.f.Upload(tt.args.bucket, tt.args.filename, tt.args.reader); (err != nil) != tt.wantErr {
 				t.Errorf("FileManager.Upload() error = %v, wantErr %v", err, tt.wantErr)
@@ -102,6 +110,40 @@ func TestFileManager_Download(t *testing.T) {
 			n, err := got.Read(buf)
 			f, err := os.Create("test.pdf")
 			f.Write(buf[:n])
+		})
+	}
+}
+
+func TestFileManager_ListItems(t *testing.T) {
+	type args struct {
+		bucket string
+		prefix string
+	}
+	tests := []struct {
+		name          string
+		f             *FileManager
+		args          args
+		wantFilenames []string
+		wantErr       bool
+	}{
+		{
+			name:          "Test List",
+			f:             New(),
+			args:          args{"preview_bucket", "test"},
+			wantFilenames: []string{"test/1", "testfile"},
+		},
+	}
+	for _, tt := range tests {
+		endpoint = "localhost:4572"
+		t.Run(tt.name, func(t *testing.T) {
+			gotFilenames, err := tt.f.ListItems(tt.args.bucket, tt.args.prefix)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FileManager.ListItems() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotFilenames, tt.wantFilenames) {
+				t.Errorf("FileManager.ListItems() = %v, want %v", gotFilenames, tt.wantFilenames)
+			}
 		})
 	}
 }

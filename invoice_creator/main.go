1e671c7abe83402187cc67e26548974989b4c43e
JESUS Matheus <matheus.jesus@alstomgroup.com>
2019-08-30 23:41:45 -0300

    invoice_creator 0.0.2

--- invoice_creator/main.go
+++ invoice_creator/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 
+	"github.com/mathantunes/arex_project/filestore"
 	"github.com/mathantunes/arex_project/invoice"
 	"github.com/mathantunes/arex_project/queuer"
 	"github.com/mathantunes/arex_project/services"
@@ -13,14 +14,28 @@ import (
 	"google.golang.org/grpc"
 )
 
-/*
-
- */
+const (
+	InvoicePreviewBucket = "preview_bucket"
+	AttachmentsBucket    = "attachment_bucket"
+)
 
 func main() {
+
+	fileManager := filestore.New()
+	//Create buckets
+	err := fileManager.CreateBucket(InvoicePreviewBucket)
+	if err != nil {
+		log.Println(err)
+	}
+	err = fileManager.CreateBucket(AttachmentsBucket)
+	if err != nil {
+		log.Println(err)
+	}
+
 	port := flag.String("port", "5000", "grpc port for listener")
 	flag.Parse()
 	runServer(fmt.Sprintf(":%v", *port))
+
 }
 
 func runServer(addr string) {
@@ -34,6 +49,8 @@ func runServer(addr string) {
 		&validator.VIESValidator{},
 		queuer.New(),
 	})
+
+	services.RegisterInvoiceGetterServer(grpcServer, &invoice.GetterServer{})
 	// pb.RegisterRouteGuideServer(grpcServer, &routeGuideServer{})
 	grpcServer.Serve(lis)
 }

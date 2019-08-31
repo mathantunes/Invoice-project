36010518b44940c8405e1a884c05008f1184d666
JESUS Matheus <matheus.jesus@alstomgroup.com>
2019-08-31 18:57:01 -0300

    client 0.0.2

--- client/getter/get_invoice_attachments_test.go
+++ client/getter/get_invoice_attachments_test.go
@@ -13,7 +13,7 @@ import (
 
 func TestGetAttachments(t *testing.T) {
 	/* INPUTS */
-	invoiceNumber := int64(10000)
+	invoiceNumber := int64(90000)
 	addr := ":6020"
 	/* INPUTS */
 
@@ -23,39 +23,12 @@ func TestGetAttachments(t *testing.T) {
 	}
 
 	client := services.NewInvoiceGetterClient(conn)
-	stream, err := client.GetAttachments(context.Background(), &services.QueryInvoice{
+	resp, err := client.GetAttachments(context.Background(), &services.QueryInvoice{
 		InvoiceNumber: invoiceNumber,
 	})
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	outputBytes := make([]byte, 1000000)
-	files := make(map[string][]byte)
-	for {
-		attachments, err := stream.Recv()
-		if err != nil {
-			if err == io.EOF {
-				goto END
-			}
-			fmt.Println(err)
-			goto END
-		}
-
-		fmt.Printf("append")
-		outputBytes = append(outputBytes, attachments.GetData()...)
-		files[attachments.GetFilename()] = outputBytes
-	}
-END:
-	fmt.Printf("END")
-	for key, value := range files {
-		f, err := os.Create(fmt.Sprintf("./testdata/%v.pdf", key))
-		if err != nil {
-			t.Error(err)
-		}
-		_, err = f.Write(value)
-		if err != nil {
-			t.Error(err)
-		}
-	}
+	t.Log(resp)
 }
